e02854acd9e46acba44313221e8b32ea11032160
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-29 12:17:00 +0200

    username to webuser changed.. but not working..

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { sequelize, Flashcard } = require("./models");
 
 const app = express();
-const port = 3068;
+const port = 3070;
 
 app.use(express.json());
 
